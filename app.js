b591150cd0fed83d0deb65d2cc5aa303dc45ae8d
ravelinejunior <junior.raveline@gmail.com>
2021-05-12 09:05:31 -0300

    Authentication with Hash md5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
+const md5 = require('md5');
 
 const app = express()
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
 
 const secret = process.env.SECRET
 
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ['password']})
+//userSchema.plugin(encrypt,{secret:secret, encryptedFields: ['password']})
 
 const User = new mongoose.model("User",userSchema)
 
@@ -45,7 +46,7 @@ app.get('/register',(req,res)=>{
 app.post('/register',(req,res)=>{
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
 
     newUser.save((err)=>{
@@ -59,11 +60,12 @@ app.post('/register',(req,res)=>{
 
 app.post('/login',(req,res)=>{
     const userName = req.body.username
-    const password = req.body.password
+    const pass = md5(req.body.password)
 
     User.findOne({email:userName},(err,findOne)=>{
         if(findOne){
-            if(findOne.password === password){
+            console.log(findOne);
+            if(findOne.password === pass){
                 console.log(findOne);
                 res.render('secrets')
             }
